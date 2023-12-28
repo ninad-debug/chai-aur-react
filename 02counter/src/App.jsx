f7fa3b27a220b602af6b57e5ffd5edad36c717df
ninad-debug <ninadsamant07@gmail.com>
2023-12-28 15:46:21 +0530

    used useState Hook for counter Application

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+  // let counter = 15;
+
+  function addValue() {
+    // counter = counter + 1;
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  }
+
+  function removeValue() {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove value - {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
